8997a9b0530e4a02368f181de365f589e796094b
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-30 13:32:59 -0400

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-
+import logoImage from '../images/cardinalsLogoOne.png';
 
 export default function Header() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setIsVisible(true);
-  }, []);
-
-  return (
-    <div className={`header--container ${isVisible ? 'fade-in-out' : ''}`}>
-      <h1 className="header--title">The Cardinals</h1>
-    </div>
-  );
-}
+   const [isVisible, setIsVisible] = useState(false);
+ 
+   useEffect(() => {
+     setIsVisible(true);
+   }, []);
+ 
+   return (
+     <div className={`header--container ${isVisible ? 'fade-in-out' : ''}`}>
+       <h1 className="header--title">The Cardinals</h1>
+         <img className="header--image"
+            src={logoImage}
+            alt="Cardinals Logo"
+         />
+     </div>
+   );
+ }
+ 
