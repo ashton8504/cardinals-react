89d8b5adbae4b005ae3728c8076da960a0f60f5d
ashton8504 <ashtonlittle8504@gmail.com>
2023-07-05 10:31:39 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header"
 import Band from "./Components/Band"
+// import SetlistSearch from "./Components/SetlistSearch";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Band />
+
     </div>
   );
 }
